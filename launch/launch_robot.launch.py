20e5c97d5469e85045613ab4a5a3796cf166e320
pi <stathisvotsis@gmail.com>
2025-05-05 18:44:43 +0300

    delete unec

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -106,11 +106,6 @@ def generate_launch_description():
     # )
     #
     # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
-    imu_driver_node = Node(
-        package="eve_bot",
-        executable="mpu6050_driver.py"
-    )
-
 
     # Launch them all!
     return LaunchDescription([
@@ -120,6 +115,5 @@ def generate_launch_description():
         twist_mux,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
-        delayed_joint_broad_spawner,
-        imu_driver_node
+        delayed_joint_broad_spawner
     ])
